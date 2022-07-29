951ee31eba4b5e0fea7288a9b35ed7b2a22708fb
kylieogg <kylieogg1@gmail.com>
2022-07-29 17:24:31 -0400

    updated thought model

--- models/Thought.js
+++ models/Thought.js
@@ -5,8 +5,36 @@ const ThoughtSchema = new Schema(
         thoughtText: {
             type: String,
             required: "A thought entry is required",
-            
+            minlength: 1,
+            maxlength: 280
 
-        }
+        },
+
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            // find getter method to formate the timestamp on query
+        },
+
+        username: {
+            type: String,
+            required: true,
+        },
+
+        reactions: [reactionSchema],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
     }
-)
+);
+
+ThoughtSchema.virtual("reactionCount").get(function() {
+    return this.reactions.length;
+});
+
+const Thought = model("Thought", ThoughtSchema);
+
+module.exports = Thought;
