9218238ee8c388279995fff0964f30c74f5bc9f2
kylieogg <kylieogg1@gmail.com>
2022-07-30 09:23:03 -0400

    updated thought controller

---
+++ controllers/thought-controller.js
@@ -0,0 +1,70 @@
+const { Thought } = require('../models');
+
+const thoughtController = {
+  // get all thoughts
+  getAllThoughts(req, res) {
+    Thought.find({})
+      .populate({
+        path: 'comments',
+        select: '-__v'
+      })
+      .select('-__v')
+      .sort({ _id: -1 })
+      .then(dbThoughtData => res.json(dbThoughtData))
+      .catch(err => {
+        console.log(err);
+        res.sendStatus(400);
+      });
+  },
+
+  // get one thought by id
+  getThoughtById({ params }, res) {
+    Thought.findOne({ _id: params.id })
+      .populate({
+        path: 'comments',
+        select: '-__v'
+      })
+      .select('-__v')
+      .then(dbThoughtData => res.json(dbThoughtData))
+      .catch(err => {
+        console.log(err);
+        res.sendStatus(400);
+      });
+  },
+
+  // createThought
+  createThought({ body }, res) {
+    Thought.create(body)
+      .then(dbThoughtData => {
+          return User.findOneAndUpdate(
+              { _id: body.userId},
+              { $push: {thoughts: dbThoughtData._id}},
+              { new: true }
+          );
+        })
+      .then((dbThoughtData) => res.json(dbThoughtData))
+      .catch(err => res.json(err));
+  },
+
+  // update thought by id
+  updateThought({ params, body }, res) {
+    Thought.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
+      .then(dbThoughtData => {
+        if (!dbThoughtData) {
+          res.status(404).json({ message: 'No thought found with this id!' });
+          return;
+        }
+        res.json(dbThoughtData);
+      })
+      .catch(err => res.json(err));
+  },
+
+  // delete thought
+  deleteThought({ params }, res) {
+    Thought.findOneAndDelete({ _id: params.id })
+      .then(dbThoughtData => res.json(dbThoughtData))
+      .catch(err => res.json(err));
+  }
+};
+
+module.exports = thoughtController;
