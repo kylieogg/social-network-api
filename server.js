90e45a5cb8869c2c5751cdc28d243f2dcc7890f6
kylieogg <kylieogg1@gmail.com>
2022-07-30 09:53:35 -0400

    updated server.js

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
+const express = require('express');
 const mongoose = require('mongoose');
 
+const app = express();
+const PORT = process.env.PORT || 3001;
 
-
-
-
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/social-network-api', {
@@ -14,3 +16,7 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/social-ne
 
 // Used to log mongo queries being executed
 mongoose.set('debug', true);
+
+app.use(require('./routes'));
+
+app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
